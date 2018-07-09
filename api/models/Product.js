268fa185a6e084942984c840a5792c0d44a6bd1a
Luber Echavarria <luberechavarria@gmail.com>
2018-07-09 18:49:55 +1000

    starting to use data-base

---
+++ api/models/Product.js
@@ -0,0 +1,18 @@
+// api/models/Product.js
+module.exports = {
+    attributes: {
+        title: {
+            type: 'string',
+            required: true
+        },
+
+        description: { 
+            type: 'string',
+        },
+
+        image: {
+            type: 'string',
+            defaultsTo: "images/lapto.jpeg"
+        }
+    },
+};
