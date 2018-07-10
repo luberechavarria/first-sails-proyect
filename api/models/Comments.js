a3fba6d6b307ee7aa7ff60c0cc5ef2944acdea62
Luber Echavarria <luberechavarria@gmail.com>
2018-07-10 19:20:40 +1000

    new model

---
+++ api/models/Comments.js
@@ -0,0 +1,25 @@
+//Api/model/comments.js
+module.export= {
+    attributes:{
+        idProduct:{
+          type:"number",
+          required:true
+        },
+
+        name:{
+          type:"string",
+          required: true
+        },
+
+        comment:{
+            type:"string",
+            required: true
+        },
+
+        imgUser:{
+            type:"strindg",
+
+        }
+
+    }
+};
